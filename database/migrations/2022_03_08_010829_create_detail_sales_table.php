a8e9895661a9d796420957116c5bf60e2dda3409
2021 DSM53 Duarte V Fernando <al222010329@gmail.com>
2022-03-07 19:37:20 -0600

    Modificamos los campos para DetalleVentas

--- database/migrations/2022_03_08_010829_create_detail_sales_table.php
+++ database/migrations/2022_03_08_010829_create_detail_sales_table.php
@@ -14,7 +14,17 @@ class CreateDetailSalesTable extends Migration
     public function up()
     {
         Schema::create('detail_sales', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->bigInteger('cantidad');
+            $table->bigInteger('subtotal');
+
+            //!LLaves foraneas
+            $table->unsignedBigInteger('sales_id'); 
+            $table->foreign('sales_id')->references('id')->on('sales');
+            $table->unsignedBigInteger('products_id');
+            $table->foreign('products_id')->references('id')->on('products');
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('users_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
