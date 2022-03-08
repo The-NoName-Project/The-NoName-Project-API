5ca01a0989bda52c97d43ef3ae487c79a68e4d1e
2021 DSM53 Duarte V Fernando <al222010329@gmail.com>
2022-03-07 19:12:12 -0600

    Creamos los modelos

---
+++ app/Http/Controllers/DetailSalesController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDetailSalesRequest;
+use App\Http\Requests\UpdateDetailSalesRequest;
+use App\Models\DetailSales;
+
+class DetailSalesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreDetailSalesRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDetailSalesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DetailSales  $detailSales
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DetailSales $detailSales)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DetailSales  $detailSales
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DetailSales $detailSales)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateDetailSalesRequest  $request
+     * @param  \App\Models\DetailSales  $detailSales
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDetailSalesRequest $request, DetailSales $detailSales)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DetailSales  $detailSales
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DetailSales $detailSales)
+    {
+        //
+    }
+}
---
+++ database/factories/DetailSalesFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DetailSalesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
