85bbbc022dec1517bb6b6b8abf0f6e9854447567
2021 DSM53 Duarte V Fernando <al222010329@gmail.com>
2022-03-16 17:02:53 -0600

    Modificacion de nombres en las rutas api para las demas rutas

--- routes/api.php
+++ routes/api.php
@@ -24,41 +24,41 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/categories/store', [CategoryController::class, 'store']);
 Route::get('/categories/{categories}', [CategoryController::class, 'show']);
-Route::get('/categories/{id}', [CategoryController::class, 'edit']);
-Route::put('/categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+Route::get('/categories/{id}/edit', [CategoryController::class, 'edit']);
+Route::put('/categories/{id}/update', [CategoryController::class, 'update']);
+Route::delete('/categories/{id}/delete', [CategoryController::class, 'destroy']);
 
         // * Rutas de la api para productos
 Route::get('/products', [ProductsController::class, 'index']);
-Route::post('/products', [ProductsController::class, 'store']);
+Route::post('/products/store', [ProductsController::class, 'store']);
 Route::get('/products/{products}', [ProductsController::class, 'show']);
-Route::get('/products/{id}', [ProductsController::class, 'edit']);
-Route::put('/products/{id}', [ProductsController::class, 'update']);
-Route::delete('/products/{id}', [ProductsController::class, 'destroy']);
+Route::get('/products/{id}/edit', [ProductsController::class, 'edit']);
+Route::put('/products/{id}/update', [ProductsController::class, 'update']);
+Route::delete('/products/{id}/delete', [ProductsController::class, 'destroy']);
 
         // * Rutas de la api para usuarios
-Route::get('/clients', [UserController::class, 'index']);
-Route::post('/clients',[UserController::class, 'store']);
-Route::get('/clients/{user}',[UserController::class, 'show']);
-Route::get('/clients/{id}',[UserController::class, 'edit']);
-Route::put('/clients/{id}',[UserController::class, 'update']);
-Route::delete('/clients/{id}',[UserController::class, 'destroy']);
+// Route::get('/clients', [UserController::class, 'index']);
+// Route::post('/clients',[UserController::class, 'store']);
+// Route::get('/clients/{user}',[UserController::class, 'show']);
+// Route::get('/clients/{id}',[UserController::class, 'edit']);
+// Route::put('/clients/{id}',[UserController::class, 'update']);
+// Route::delete('/clients/{id}',[UserController::class, 'destroy']);
 
         // * Rutas de la api para ventas
-Route::post('/sales',[SalesController::class, 'store']);
 Route::get('/sales',[SalesController::class, 'index']);
+Route::post('/sales/store',[SalesController::class, 'store']);
 Route::get('/sales/{sale}',[SalesController::class, 'show']); //Mostrar una sola venta
-Route::get('/sales/{id}',[SalesController::class, 'edit']);
-Route::put('/sales/{id}',[SalesController::class, 'update']);
-Route::delete('/sales/{id}',[SalesController::class, 'destroy']);
+Route::get('/sales/{id}/edit',[SalesController::class, 'edit']);
+Route::put('/sales/{id}/update',[SalesController::class, 'update']);
+Route::delete('/sales/{id}/delete',[SalesController::class, 'destroy']);
 
         // * Rutas de la api para detalles de ventas
-Route::post('/detailsales',[DetailSalesController::class, 'store']);
 Route::get('/detailsales',[DetailSalesController::class, 'index']);
+Route::post('/detailsales/store',[DetailSalesController::class, 'store']);
 Route::get('/detailsales/{deta}',[DetailSalesController::class, 'show']);
-Route::get('/detailsales/{id}',[DetailSalesController::class, 'edit']);
-Route::put('/detailsales/{id}',[DetailSalesController::class, 'update']);
-Route::delete('/detailsales/{id}',[DetailSalesController::class, 'destroy']);        
+Route::get('/detailsales/{id}/edit',[DetailSalesController::class, 'edit']);
+Route::put('/detailsales/{id}/update',[DetailSalesController::class, 'update']);
+Route::delete('/detailsales/{id}/delete',[DetailSalesController::class, 'destroy']);        
 
 //Rutas de la api para el login de usuarios
 Route::post('/login', [AuthController::class, 'login']);
